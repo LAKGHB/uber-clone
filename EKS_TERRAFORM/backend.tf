410ca2f43009fcdee15bdb47eb74f598fdb7f11a
Ramu <65284653+LAKGHB@users.noreply.github.com>
2024-01-04 20:42:06 +0000

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "anil-uberclone" # Replace with your actual S3 bucket name
+    bucket = "anil-uberclone1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
